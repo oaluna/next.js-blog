a879715e7317ed1138713187d35fa2a3f26e4acc
Oscar Luna <CaporalesLuna@gmail.com>
2020-07-28 17:17:21 -0700

    Moved files up one level in tree; Removed next.js-blog directory

---
+++ pages/first-post.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+
+const FirstPost = () => (
+  <>
+    <h1>My Journey Into Tech</h1>
+    <h2>From Hiding Being Self-Taught to an Actual Job in Tech</h2>{' '}
+    <p>
+      Before I begin this lengthy, insane story, let me start by saying that if
+      you, the reader, are in a situation that makes obtaining a career in tech
+      seem like a lofty goal, I ask that you slap yourself in the face and in
+      your best Cher voice, say to yourself, "Snap out of it!" Read my story and
+      then reflect on your own capabilities of making a desired change in your
+      life.
+    </p>{' '}
+    <p>
+      It isn't uncommon to hear about unconventional backgrounds when hearing
+      stories from other web developers. My unconventional background, I feel,
+      can be more appropriately described as haphazard. I'm a college dropout
+      who dedicated his twenties trying to find stardom as a dancer while
+      supporting this dream with multiple jobs as a cafe cashier. I had stars in
+      my eyes and tech was nowhere in sight in my radar.
+    </p>
+    <p>TBC</p>
+  </>
+)
+
+export default FirstPost
